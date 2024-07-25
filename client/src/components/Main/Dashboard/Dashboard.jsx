7f6a6644ce55981afa33fa845561c7ae03f4cdb6
EduFatou <edufatou@gmail.com>
2024-07-25 20:36:20 +0200

    wip: saving changes before pull develop

--- client/src/components/Main/Dashboard/Dashboard.jsx
+++ client/src/components/Main/Dashboard/Dashboard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import SearchDashboard from "./SearchDashboard/SearchDashboard";
+import Redirector from './Redirector';
+import Overview from './Overview';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return <section className="dashboard">
+    <article>
+      <SearchDashboard />
+      <Redirector />
+      <Overview />
+    </article>
+  </section>
 };
 
 export default Dashboard;
--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import LoginGoogle from "./LoginGoogle/LoginGoogle";
+import Dashboard from "./Dashboard/Dashboard";
 
 const Main = () => {
   return (
   <main className="main">
     <Routes>
       <Route path="/login" element={<LoginGoogle />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   </main>
   );
